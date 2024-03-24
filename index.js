0432f726642e51b2758b8fbe38bb053657537d0f
MutaiNewton56 <mutainewton56.gmail.com>
2024-03-24 18:48:41 +0300

    Arithmetic lab complete

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-
 // Write your code here
+
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+// Challenge 2: Random
+let random = Math.floor(Math.random() * 100) + 1;
+
+// Challenge 3: Mod
+let num3 = 14; // Change num3 value to 14
+let num4 = 5;  // Change num4 value to 5
+let mod = num3 % num4;
+
+// Challenge 4: Max
+let max = Math.max(1, 2, 3, 4, 5, 10, 15, 20, 15);
